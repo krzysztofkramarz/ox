628b9cbf6c22d2ac65e8381f9d7fb5553a359599
Krzysztof Kramarz <krzysztof.kramarz@silvermedia.pl>
2019-03-20 09:18:29 +0100

    Board refactor

--- src/main/java/com/fonowizja/ox/Board.java
+++ src/main/java/com/fonowizja/ox/Board.java
@@ -1,8 +1,13 @@
 package com.fonowizja.ox;
 
 /**
+ * Place for acting single play
  * @author krzysztof.kramarz
  */
 class Board
 {
+   public void setSize(int x, int y)
+   {
+
+   }
 }
--- src/test/java/com/fonowizja/ox/BoardTest.java
+++ src/test/java/com/fonowizja/ox/BoardTest.java
@@ -1,8 +1,29 @@
 package com.fonowizja.ox;
 
+import java.util.Arrays;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 /**
  * @author krzysztof.kramarz
  */
-class BoardTest
+public class BoardTest
 {
+   Board board;
+
+   @BeforeTest
+   public void setUp()
+   {
+      board = new Board();
+   }
+
+   @Test
+   public void testBoardCells()
+   {
+
+      board.setSize(5, 5);
+   }
+
+
 }
